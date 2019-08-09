5209d01acc6a748d1885e86e86fb4fbedfee23ad
naman815 <naman815@gmail.com>
2019-08-09 22:24:04 +0530

    title change

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,7 +3,7 @@
 module.exports.home = function(req,res){
 
     return res.render('home',{  // rendering home.ejs file via home_controller
-        title : "ShopNani",     // sending title to ejs file
+        title : "Flipkart Search",     // sending title to ejs file
         data: '',
     })
 }
